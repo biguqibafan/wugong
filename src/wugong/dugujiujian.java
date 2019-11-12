09529132acc6c5d56906d6dca3afdf0f74beb94f
yueluochen <yueluochen@foxmail.com>
2019-11-12 20:31:49 +0800

    更新3

--- src/wugong/dugujiujian.java
+++ src/wugong/dugujiujian.java
@@ -4,6 +4,6 @@ public class dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("破剑式：亢龙有悔");
-		System.out.println("破刀式：六脉神剑");
+		System.out.println("破箭式：漫天花雨");
 	}
 }
